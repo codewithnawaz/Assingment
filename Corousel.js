f66f75bcd0b63e3594967e0e43d201762f9d9e8f
codewithnawaz <97677033+codewithnawaz@users.noreply.github.com>
2022-09-30 01:04:36 +0530

    Add files via upload

---
+++ Corousel.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function Corousel() {
+        return (
+
+                <div className="CONTAINER">
+                        <div className="box 1">
+                                <img src="./images/beweglichkeit.jpg" className="d-block w-100 h-100" alt="..." />
+                                <div class="text-block">
+                                        <h4>MOBILITY</h4>
+                                </div>
+                        </div>
+                        <div className="box 2">
+                                <img src="./images/male-lifting.jpg" className="d-block w-100 h-100" alt="..." />
+                                <div class="text-block">
+                                        <h4>STRENGTH</h4>
+                                </div>
+                        </div>
+                        <div className="box 3">
+                                <img src="./images/drill.jpg" className="d-block w-100 h-100" alt="..." />
+                                <div class="text-block">
+                                        <h4>DRILLS</h4>
+                                </div>
+                        </div>
+                </div>
+                
+
+        )
+
+}
+
+
--- Navbar.js
+++ Navbar.js
@@ -2,27 +2,41 @@ import React from 'react'
 
 export default function Navbar() {
     return (
-        <nav class="navbar navbar-expand-lg bg-light ">
-            <div class="container-fluid">
-                <a class="navbar-brand " href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg bg-light ">
+            <div className="container-fluid">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul className="navbar-nav mx-auto">
+                        <li className="nav-item mx-3">
+                        <img src="./images/webcam.svg" className='mx-1'/>
+                            <a className="nav-link" aria-current="page" href="#">LIVE</a>
+                            
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Features</a>
+                        <li className="nav-item mx-3">
+                        <img src="./images/clipboard2-check.svg" className='mx-4'/>
+                            <a className="nav-link" href="#">MEMBERSHIPS</a>
+                           
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Pricing</a>
+                        <li className="nav-item mx-3">
+                        <img src="./images/graph-up-arrow.svg" className='mx-2'/>
+                            <a className="nav-link" href="#">PLANS</a>
+                            
+                        </li>
+                        <li className="nav-item mx-3">
+                        <img src="./images/grid.svg" className='mx-2'/>
+                            <a className="nav-link" href="#">MORE</a>
+                            
                         </li>
                     </ul>
+                    <ul className="nav-item mx-3 ">
+                    <img src="./images/person-fill.svg" />
+                        <a className="nav-link" href="#">log In</a>
+                    </ul>
+
                 </div>
             </div>
         </nav>
     )
 }
-
